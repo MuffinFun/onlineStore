e5bdf31c1f6e2e4170b9e347c8ed268ef0476de4
macOs <max.kriv3228@gmail.com>
2024-02-09 13:47:42 +0300

    create universal error hadler middleware

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,14 @@
+const ApiError = require('../error/ApiEror');
+
 class UserController {
   async registration(req, res) {}
   async login(req, res) {}
-  async check(req, res) {
-    res.json('aaaaaa');
+  async check(req, res, next) {
+    const { id } = req.query;
+    if (!id) {
+      return next(ApiError.badRequest('Id does not exist'));
+    }
+    res.json(id);
   }
 }
 
