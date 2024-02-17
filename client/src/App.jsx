aaf6b0db87e36c97bc6a080a7ab1ef775d81881b
macOs <max.kriv3228@gmail.com>
2024-02-17 09:19:26 +0300

    base construct

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { createContext } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './components/AppRouter.jsx';
 import UserStore from './store/UserStore.js'
+import DeviceStore from './store/DeviceStore.js';
 
 export const Context = createContext(null) 
 
@@ -12,7 +13,8 @@ function App() {
 
   return (
     <Context.Provider value={{
-      user: new UserStore()
+      user: new UserStore(),
+      devices: new DeviceStore()
     }}>
       <BrowserRouter>
         <AppRouter/>
---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { Context } from "../../App";
+
+const NavBar = () =>{
+
+    const {user} = useContext(Context)
+
+    return (
+        <></>
+    )
+}
+
+export default NavBar;
