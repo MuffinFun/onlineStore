0d1ce771ff8061a595ae348ca48b9f51414291a4
macOs <max.kriv3228@gmail.com>
2024-02-26 18:34:40 +0300

    history redirection

--- client/src/components/DeviceList/DeviceItem.jsx
+++ client/src/components/DeviceList/DeviceItem.jsx
@@ -1,10 +1,13 @@
 import { Card, Col, Image } from "react-bootstrap";
 import star from '../../assets/images/star.png'
 import '../../assets/css/components/Devices.css'
+import { DEVICE_ROUTE } from "../../utils/consts";
+import { useNavigate } from "react-router-dom";
 
 const DeviceItem = ({device}) => {
+    const history = useNavigate()
   return (
-    <Col md='3'>
+    <Col md='3' onClick={()=>history(DEVICE_ROUTE + "/" + device.id)}>
         <Card className="device-item">
             <Image className="device-item__img" src={device.img}/>
             <div className="d-flex justify-content-between align-items-center device-item__bottom">
