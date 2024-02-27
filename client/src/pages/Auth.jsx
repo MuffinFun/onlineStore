b43c7cf1df24c9b9efc6833d42b5d3225a895558
macOs <max.kriv3228@gmail.com>
2024-02-27 19:44:15 +0300

    userAPI and index .js create

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -2,11 +2,26 @@ import '../assets/css/pages/auth.css'
 import {Container, Card, Form, Button, Row} from "react-bootstrap";
 import {NavLink, useLocation} from 'react-router-dom';
 import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts';
+import { login, registration } from '../http/userAPI.js';
+import { useState } from 'react';
 
 const Auth = ()=> {
   const Location = useLocation()
 
   const isLogin = Location.pathname === LOGIN_ROUTE;
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const click = async ()=>{
+
+    if(isLogin){
+      const response = await login()
+    }else{
+      const response = await registration(email, password)
+      console.log(response)
+    }
+  }
 
   return (
   <>
@@ -17,10 +32,15 @@ const Auth = ()=> {
           <Form.Control
             className='mt-3'
             placeholder='input your Email...'
+            value={email}
+            onChange={e=>setEmail(e.target.value)}
           />
           <Form.Control
             className='mt-3'
             placeholder='input your Password...'
+            type='password'
+            value={password}
+            onChange={e=>setPassword(e.target.value)}
           />
           <Row className='d-flex justify-content-between align-items-center auth-form__bot'>
             {isLogin ? 
@@ -32,7 +52,11 @@ const Auth = ()=> {
               Already have an Accout? <NavLink to={LOGIN_ROUTE}>LogIn!</NavLink>
             </div>
             }
-            <Button className="auth-form__btn" variant={"outline-success"}>
+            <Button 
+              className="auth-form__btn" 
+              variant={"outline-success"}
+              onClick={click}
+            >
               {isLogin ? 'Login' : 'Registration'}
             </Button>
           </Row>
